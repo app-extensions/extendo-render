8ab10acb220202c699c3bdc2778dbd402b709213
Jeff McAffer <jeffmcaffer@github.com>
2021-03-21 22:21:19 -0700

    sprinkle magic console.log debug dust

--- mermaid/render.js
+++ mermaid/render.js
@@ -15,9 +15,10 @@ const defaultConfig = { theme: 'default' }
 const defaultBackground = 'white'
 const defaultViewPort = { width: 800, height: 600, deviceScaleFactor: 1 }
 
+const log = []
+
 async function createBrowser(mode) {
   if (mode === 'lambda') {
-    console.log('1 ...')
     const chromium = require('chrome-aws-lambda');
     const params = {
       args: chromium.args,
@@ -34,54 +35,59 @@ async function createBrowser(mode) {
 }
 
 async function render(definition, inputs) {
-  console.log('In runner...')
   const browser = await createBrowser(process.env.MODE || 'local')
-  console.log('2 ...')
+  log.push('1 ...')
   try {
     const page = await browser.newPage();
-    console.log('3 ...')
+    log.push('3 ...')
     page.setViewport({ ...defaultViewPort, ...inputs.viewPort });
     await page.goto(`file://${path.join(__dirname, './node_modules/@mermaid-js/mermaid-cli', 'index.html')}`);
-    console.log('4 ...')
+    log.push('4 ...')
     await page.evaluate(`document.body.style.background = '${inputs.backgroundColor || defaultBackground}'`);
+    log.push('5 ...')
 
     const setup = (container, definition, config) => {
-      console.log('in setup ...')
+      log.push('in setup ...')
       container.textContent = definition;
       window.mermaid.initialize(config);
+      log.push('after initialize ...')
       try {
         window.mermaid.init(undefined, container);
+        log.push('after init ...')
         return { status: 'success' };
       } catch (error) {
+        log.push('error in setup ...' + error.message)
         return { status: 'error', error, message: error.message };
       }
     }
     const config = { ...defaultConfig, ...inputs.config }
+    log.push('before $eval 1 ...')
     const result = await page.$eval('#container', setup, definition, config);
     if (result.status === 'error') throw new Error(result.message);
 
     // await before returning to be sure we're good before the finally
+    log.push('before $eval 2 ...')
     const svg = await page.$eval('#container', container => container.innerHTML)
     return svg
   } finally {
+    log.push('closing ...')
     await browser.close();
   }
 }
 
-console.log('starting')
 module.exports = async () => {
-  console.log('running')
   try {
     const inputData = await fs.readFile(inputFile)
-    console.log(inputData)
+    log.push(inputData)
     const { content, inputs } = JSON.parse(inputData)
     const svg = await render(content, inputs)
+    log.push('back from render ...')
     console.log(svg)
     const response = { html: svg }
     await fs.writeFile(outputFile, JSON.stringify(response))
   } catch (error) {
-    console.log(error.message)
-    await fs.writeFile(errorFile, JSON.stringify({ error }, null, 2))
+    log.push(error.message)
+    await fs.writeFile(errorFile, JSON.stringify({ error, log }, null, 2))
     process.exit(1)
   }
 }
