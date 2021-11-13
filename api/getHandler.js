bbee9e050c5ec1af5236b635c02587dd05dea615
Jeff McAffer <jeffmcaffer@github.com>
2021-11-13 03:01:03 +0000

    add bit o debugging in get

--- api/getHandler.js
+++ api/getHandler.js
@@ -1,5 +1,6 @@
-async ({ inputs, api }) => {
+async ({ inputs, api, log }) => {
   const { resource, params, path } = inputs
   const current = await api.keyValue.get(path)
+  log.info(`The current value is: ${current}`)
   return `Hello ${params.who} from ${resource.typeName} ${resource.name}\nThe value at path: "${path}" is:\n${JSON.stringify(current)}\n`
 }
