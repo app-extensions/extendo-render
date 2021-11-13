b66fd76e3244aba33b9db7989e1a5d972e5174c6
Jeff McAffer <jeffmcaffer@github.com>
2021-11-13 02:47:49 +0000

    add POST handler for api app

--- api/getHandler.js
+++ api/getHandler.js
@@ -1,4 +1,4 @@
 ({ inputs }) => {
   const { resource, params, path } = inputs
-  return `Hello ${params.who} from ${resource.typeName} ${resource.name} with path: ${path}`
+  return `Hello ${params.who} from ${resource.typeName} ${resource.name} with path: "${path}""`
 }
---
+++ api/postHandler.js
@@ -0,0 +1,7 @@
+({ inputs, api, log }) => {
+  const { path, body } = inputs
+  const old = await api.keyValue.get(path)
+  await api.keyValue.set(path, body)
+  log.info(`Setting value for ${path} to ${JSON.stringify(body)}`)
+  return old
+}
