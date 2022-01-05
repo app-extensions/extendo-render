c3e62eb05dcc5c0b37ca068779074ebbba825a03
Jeff McAffer <jeffmcaffer@github.com>
2022-01-05 21:13:51 +0000

    move api app to it's own repo

--- api/getHandler.js
+++
@@ -1,6 +0,0 @@
-async ({ inputs, api, log }) => {
-  const { resource, params, path } = inputs
-  const current = await api.keyValue.get(path)
-  log.info(`The current value is: ${current}`)
-  return `Hello ${params.who} from ${resource.typeName} ${resource.name}\nThe value at path: "${path}" is:\n${JSON.stringify(current)}\n`
-}
--- api/postHandler.js
+++
@@ -1,7 +0,0 @@
-async ({ inputs, api, log }) => {
-  const { path, body } = inputs
-  const old = await api.keyValue.get(path)
-  await api.keyValue.set(path, body)
-  log.info(`Setting value for ${path} to ${JSON.stringify(body)}`)
-  return old
-}
